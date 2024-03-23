c8b837eacfd2077b06fe2aa6316574533fdf284b
Rafael <mirzoyevrr.03@gmail.com>
2024-03-23 21:53:37 +0300

    + models

--- web/models.py
+++ web/models.py
@@ -1,3 +1,22 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class Genre(models.Model):
+    genre_name = models.CharField(max_length=64)
+
+
+class Book(models.Model):
+    book_name = models.CharField(max_length=64)
+    is_finished = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    genres = models.ManyToManyField(Genre)
+
+
+class Summary(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    summary = models.CharField(max_length=2048)
+    reading_date = models.DateField()
